11cbe5fdb5db8bf6b42e99d3a840690e60f25ddb
Chris Beck <beck.chris88@outlook.com>
2022-11-05 22:09:59 -0500

    Updating kernel.py create_working_env and save_last_good_kernel_config

--- kernel.py
+++ kernel.py
@@ -8,7 +8,9 @@ class Kernel:
     def __init__(self):
         self.kernel_linux_folder = r"/usr/src/linux"
         self.kernel_linux_config_backup_folder = "kernel_config"
+        self.last_working_kernel_dir = "last_working_config"
         self.date_time_stamp_string = None
+        self.log_file_location = None
 
     
     def get_time_stamp(self):
@@ -27,6 +29,7 @@ class Kernel:
             destFolderName = os.path.join(self.kernel_linux_folder, self.kernel_linux_config_backup_folder)
             destFileName = os.path.join(destFolderName, fileName)
             self.create_working_env()
+            self.save_last_good_kernel_config()
             shutil.copyfile(r"/usr/src/linux/.config", destFileName)
             if os.path.isfile(destFileName):
                 print("Everything worked as expected")
@@ -35,15 +38,46 @@ class Kernel:
     
     def create_working_env(self):
         desired_path = os.path.join(self.kernel_linux_folder, self.kernel_linux_config_backup_folder)
-        if not os.path.isdir(desired_path):
+        working_path = os.path.join(self.kernel_linux_folder, self.last_working_kernel_dir)
+        if not os.path.isdir(desired_path) or not os.path.isdir(working_path):
+            # I hate the way this looks. Todo, break this into two seperate checks instead of one so I do not have to worry if a file is already created. Other wise I get a [Errno 17] File exists:
+            print("creating config dir")
+            try:
+                os.mkdir(desired_path)
+            except:
+                pass
             print("creating working dir")
-            os.mkdir(desired_path)
+            try:
+                os.mkdir(working_path)
+            except:
+                pass
         else:
-            print("No need to create working directory because it already exists")
+            print("No need to create working/config directory because it already exists")
     
+    def save_last_good_kernel_config(self):
+        path = os.path.join(self.kernel_linux_folder, self.last_working_kernel_dir)
+        print("Is the current config file a working kernel config? (y/n) ")
+        user_response = input()
+        lowered_user_response = user_response.lower()
+        if lowered_user_response == "y":
+            orginal_config_file = os.path.join(self.kernel_linux_folder, ".config")
+            backup_config_file = os.path.join(path, ".config")
+            # this should do some sort of check to verify it wrote correclty but I am not wanting to do this today
+            shutil.copyfile(orginal_config_file, backup_config_file)
+        else:
+            pass
+
     def find_latest_backup_config(self):
+        folderPath = os.path.join(self.kernel_linux_folder, self.kernel_linux_config_backup_folder)
+        all_files = os.listdir(folderPath)
+        config_files = []
+        for all_file in all_files:
+            if all_file.startswith(".config"):
+                config_files.append(all_file)
         pass
+
 
 
 kernelObj = Kernel()
 kernelObj.backup_kernel_config()
+#kernelObj.find_latest_backup_config()
